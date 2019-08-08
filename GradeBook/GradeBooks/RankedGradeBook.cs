b259e24e4bcc241f3623774b0b04b88c29b5d246
gabriel.melchor@softtek.com <gabriel.melchor@softtek.com>
2019-08-08 10:35:01 -0700

    Avg

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,68 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            List<double> GradeStudentList = new List<double>;
+            var studentSize = Students.Count;
+            if (studentSize < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            foreach (var student in Students)
+            {
+                GradeStudentList.Add(student.AverageGrade);
+            }
+
+            GradeStudentList.Sort();
+            GradeStudentList.Reverse();
+
+            int position = 0;
+            foreach (var grade in GradeStudentList)
+            {
+                if (averageGrade <= grade)
+                {
+                    position++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var top20 = (20 * studentSize) / 100;
+            if(position <= top20)
+            {
+                return 'A';
+            }
+            else
+            {
+                top20 += top20;
+                if(position <= top20)
+                {
+                    return 'B';
+                }
+                else
+                {
+                    top20 += top20;
+                    if (position <= top20)
+                    {
+                        return 'C';
+                    }
+                    else
+                    {
+                        top20 += top20;
+                        if (position <= top20)
+                        {
+                            return 'D';
+                        }
+                    }
+                }
+            }
+
+            return 'F';
+        }
     }
 }
